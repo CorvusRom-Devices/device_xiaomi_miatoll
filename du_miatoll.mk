57eaaaa5c847f5eefab943973f65c3f401df8369
AtlanPrime <zilliondollarkuruvi@gmail.com>
2020-09-12 09:18:54 +0000

    miatoll: Enable Google Recorder for all

--- du_miatoll.mk
+++ du_miatoll.mk
@@ -7,6 +7,9 @@
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 $(call inherit-product, vendor/du/config/common_full_phone.mk)
 
+# Google Recorder
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
 PRODUCT_NAME := du_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
