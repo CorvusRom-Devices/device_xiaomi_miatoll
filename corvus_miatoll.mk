593820648562335227274b05c63711137e9f68c3
Manavjit (Kuruin) <manavjit9@gmail.com>
2022-06-24 05:00:26 +0000

    miatoll: Corvus Bringup

--- corvus_miatoll.mk
+++ corvus_miatoll.mk
@@ -12,26 +12,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Inherit from miatoll device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
-$(call inherit-product, vendor/aosp/common.mk)
+
+# Inherit some common corvus stuff.
+$(call inherit-product, vendor/corvus/config/common.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := aosp_miatoll
+PRODUCT_NAME := corvus_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6250
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
-# Set this flag in build script
-ifeq ($(CURRENT_BUILD_TYPE), gapps)
-# Use Gapps
-TARGET_SHIPS_SEPERATE_GAPPS_BUILD := true
-WITH_GAPPS := true
-TARGET_GAPPS_ARCH := arm64
-IS_PHONE := true
-endif
+# Gapps
+USE_GAPPS=true
+
+# Corvus Maintainer
+CORVUS_MAINTAINER=ManavjitSingh
